bdd4f97c2de1de38367f395e1e8c8c831e7e43fd
Seth Cohen <smcohen82@gmail.com>
2021-02-09 18:06:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |k, v|
+    v.each do |inner_key, names|
+      names.each do |name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        
+        if !new_hash[name][k]
+          new_hash[name][k] = []
+        end
+        
+        new_hash[name][k] << inner_key.to_s
+        
+      end
+    end
+  end
+  new_hash
 end
+
