ad2a5d58f2fa5894deeeb29bca961241ff230f59
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-18 23:31:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+#require 'pry'
 def nyc_pigeon_organizer(data)
+  the_end = data.each_with_object({}) do |(key, value), accumulatorarr|
+    #binding.pry
+    value.each do |insidekey, names|
+      names.each do |lastname|
+        if !accumulatorarr[lastname]
+          accumulatorarr[lastname] = {}
+        end
+            if !accumulatorarr[lastname][key]
+              !accumulatorarr[lastname][key] = []
+        end
+accumulatorarr[lastname][key].push(insidekey.to_s)
+        end
+    end
+    #accumulatorarr
+  end
+  #binding.pry
   # write your code here!
 end
